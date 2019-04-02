591b791e0b88a6605d0e28f4cfb1178e2bf7f8ee
Lichen Zhang <qzwlecr@gmail.com>
2019-04-03 01:43:37 +0800

    add: scripts for running on cluster.

---
+++ build.sh
@@ -0,0 +1,11 @@
+module load compilers/gcc/7.4.1-low
+module load compilers/icc/intel-cc
+module load librarys/glog
+
+rm -rf build log
+mkdir build
+mkdir log
+cd build
+
+CC=icc CXX=icpc cmake ..
+make |& tee ../log/build.log
